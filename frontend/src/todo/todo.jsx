44fa079cd66ab152b6a3ed0542c12d35b10ae086
Jefferson Daniel <jeffersondanielss@gmail.com>
2017-06-28 22:31:01 -0300

    post and get

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+
 import PageHeader from '../template/pageHeader'
 import TodoForm from './todoForm'
 import TodoList from './todoList'
+
+const URL = 'http://localhost:3003/api/todos'
 
 export default class Todo extends Component {
 
@@ -10,6 +14,12 @@ export default class Todo extends Component {
     this.state = { description: '', list: [] }
     this.handleAdd = this.handleAdd.bind(this)
     this.handleChange = this.handleChange.bind(this)
+    this.refresh()
+  }
+
+  refresh() {
+    axios.get(`${URL}?short=-createdAt`)
+      .then( resp => this.setState({ ...this.state, description: '', list: resp.data }) )
   }
 
   handleChange(event) {
@@ -17,7 +27,9 @@ export default class Todo extends Component {
   }
 
   handleAdd() {
-    this.setState({ description: ''})
+    const description = this.state.description;
+    axios.post(URL, { description })
+      .then( resp => this.refresh() )
   }
 
   render() {
@@ -25,7 +37,7 @@ export default class Todo extends Component {
       <div>
         <PageHeader name="tarefas" small="Cadastro"></PageHeader>
         <TodoForm description={ this.state.description } handleChange={this.handleChange} handleAdd={ this.handleAdd }/>
-        <TodoList />
+        <TodoList list={ this.state.list } />
       </div>
     )
   }
--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
 
-export default props => (
-  <div>
-    <h1>List</h1>
-  </div>
-)
+export default props => {
+  const renderRows = () => {
+
+    const list = props.list || []
+    
+    return list.map(todo => (
+      <tr key={ todo._id }>
+        <td>{ todo.description }</td>
+      </tr>
+    ))
+  }
+
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Descrição</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        { renderRows() }
+      </tbody>
+    </table>
+  )
+}
