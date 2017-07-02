90cc1ee07c32e7354f28c5daa3e5b1feedf0a73d
Jefferson Daniel <jeffersondanielss@gmail.com>
2017-07-01 22:49:36 -0300

    fix layout

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -14,16 +14,18 @@ export default props => {
 
   return (
     <div role="form" className="todoForm">
-    <Grid cols="12 9 10">
-      <input type="text" className="form-control" id="description" placeholder="Adicione uma tarefa" onKeyUp={keyHandler}onChange={ props.handleChange } value={ props.description } />
-    </Grid>
+      <div className="row">
+        <Grid cols="12 9 10">
+          <input type="text" className="form-control" id="description" placeholder="Adicione uma tarefa" onKeyUp={keyHandler}onChange={ props.handleChange } value={ props.description } />
+        </Grid>
 
-    
-    <Grid cols="12 3 2">
-      <Button style="primary" icon="plus" onClick={ props.handleAdd }></Button>
-      <Button style="info" icon="search" onClick={ props.handleSearch }></Button>
-      <Button style="default" icon="close" onClick={ props.handleClear }></Button>
-    </Grid>
-  </div>
+        
+        <Grid cols="12 3 2">
+          <Button style="primary" icon="plus" onClick={ props.handleAdd }></Button>
+          <Button style="info" icon="search" onClick={ props.handleSearch }></Button>
+          <Button style="default" icon="close" onClick={ props.handleClear }></Button>
+        </Grid>
+      </div>
+    </div>
   )
 }
